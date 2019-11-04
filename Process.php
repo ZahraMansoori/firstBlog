fbeac704b4f6c2f9bc0d6ff6e6ad222a33b0c7d4
Mansoori <oops.rosha@yahoo.com>
2019-11-04 20:55:24 +0330

    debug1

--- Process.php
+++ Process.php
@@ -2,18 +2,16 @@
 
 $fileName = rand(100, 999) . '-' . $_FILES['upload']['name'];
 $filePath = __DIR__ . '/' . $fileName;
-$allowedType = array('image/png', 'image/jpg');
-var_dump($_FILES['upload']);
+//var_dump($_FILES['upload']);
 
+$_FILES['upload']['type'] = substr_replace($_FILES['upload']['type'], 'jpg', 6);
+echo $_FILES['upload']['type'];
+if (move_uploaded_file($_FILES['upload']['tmp_name'], $filePath)) {
+    echo "The file uploaded successfully";
+} else {
+    echo "There is something wrong on file path";
+}
 
-if (in_array($_FILES['upload']['type'], $allowedType)){
-    if (move_uploaded_file($_FILES['upload']['tmp_name'], $filePath)) {
-        echo "The file uploaded successfully";
-    } else {
-        echo "There is something wrong on file path";
-    }
-}else {
-    echo "there is something wrong on file type";}
 
 //foreach ($_REQUEST as $key => $value) {
 //    if (is_array($value)) {
